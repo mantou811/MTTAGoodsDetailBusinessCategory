1bba3d829347f1b9e0093c38401aa08810ca9b66
森 <mori@sens-Mac-mini.local>
2017-12-28 14:55:53 +0800

    Initial Commit

---
+++ MTTAGoodsDetailBusinessCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTTAGoodsDetailBusinessCategory
+//
+//  Created by 森 on 2017/12/28.
+//  Copyright © 2017年 Za. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
